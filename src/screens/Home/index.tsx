771eb9d9c17debe9c81f19b02415b99080bc3ee8
mateuskuritza <mateuskuritza@gmail.com>
2023-04-02 00:06:42 -0300

    understanding inputs and touch area

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+
+    function handleParticipantAdd() {
+        console.log('Adicionar participante')
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>Nome do Evento</Text>
+            <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6b6b6b"
+                    // Some keyboard types will only work on Android or iOS
+                    keyboardType='default'
+                />
+
+                {/* Clickeable region  */}
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+// 
